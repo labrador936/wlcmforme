2ff238de70cbb4c2f14cd64a59de55c862627cc7
labrador936 <40704323+labrador936@users.noreply.github.com>
2018-11-22 01:31:50 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,67 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!'
 
+console.log('Logged in ${client.user.tag} ')
 client.on('ready', () => {
-  client.user.setGame('w!setup for setup and w!invite for invite')
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`By Gaming Dating Amanda |`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log(`[Start] ${new Date()}`);
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log('')
+  console.log('+[------------------------------------]+');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('+[------------------------------------]+')
+  console.log('')
+  console.log('+[------------]+')
+  console.log(' Bot Is Online')
+  console.log('+[------------]+')
+  console.log('')
+  console.log('')
 });
 
-client.on('message', msg => {
-  if (msg.content === 'w!setup') {
-    msg.reply('You need to do 2 things to set up this bot! First you want to make 2 channels named welcome and good-bye. And then make a role named Regular.');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'w!invite') {
-    msg.reply('https://tiny.cc/welcomerbot');
-  }
-});
 
 client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
-    let memberavatar = member.user.avatarURL
-     if (!channel) return;
-     const embed = new Discord.RichEmbed()
-     .setColor('RANDOM')
-     .setThumbnail(memberavatar)
-     .addField('\:bust_in_silhouette: / User: ', `${member}`)
-     .addField('\:thumbsup: / Welcome here', `${member}`)
-     .addField('\:point_right: / Here you go. A role to you', `Regular`)
-     .setFooter(`Message sent by Welcomer`)
-     .setTimestamp()
-  
-     channel.sendEmbed(embed);
-  
-  
-    var role = member.guild.roles.find('name', 'Regular');
-  
-    member.addRole(role)
-  });
-
-  client.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', 'good-bye');
-    let memberavatar = member.user.avatarURL
-     if (!channel) return;
-     const embed = new Discord.RichEmbed()
-     .setColor('RANDOM')
-     .setThumbnail(memberavatar)
-     .addField('\:bust_in_silhouette: / User: ', `${member}`)
-     .addField('\:thumbsup: / Goodbye!', `${member}`)
-     .addField('\:point_right: / Now we are removing his role', `lol`)
-     .setFooter(`Message sent by Welcomer`)
-     .setTimestamp()
-  
-     channel.sendEmbed(embed);
-  
-  
-    var role = member.guild.roles.find('name', 'Regular');
-  
-    member.addRole(role)
-  });
-
-client.on('ready', () => {
-    console.log('Tjena Derpy! Boten är klar nu!')
-
+const codes = member.guild.channels.get("491307189646917672");//???? ?????
+if(!codes) return;
+if(codes) {
+codes.send(`**__Welcome To Gaming & Meeting 24/7 __ ${member}**`);          
+}
 });
 
-client.login('process.env.BOT_TOKEN');
+
+
+
+client.login(process.env.BOT_TOKEN);
